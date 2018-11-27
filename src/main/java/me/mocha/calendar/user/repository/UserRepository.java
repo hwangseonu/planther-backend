b8322286a404afa07cc2da10e03084656bbead65
hwangseonu <hwangseonu12@naver.com>
2018-11-27 19:23:59 +0900

    rebuild: Remove all file to rebuild

--- src/main/java/me/mocha/calendar/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package me.mocha.calendar.user.repository;
-
-import me.mocha.calendar.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- src/main/java/me/mocha/calendar/user/response/SignUpResponse.java
+++
@@ -1,9 +0,0 @@
-package me.mocha.calendar.user.response;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class SignUpResponse {
-}
