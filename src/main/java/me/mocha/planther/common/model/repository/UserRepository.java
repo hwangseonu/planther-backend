ca0531f66f8df380c1b39fa0c0e3f92c12279d15
hwangseonu <hwangseonu12@naver.com>
2018-12-25 20:35:48 +0900

    feat: Add user repository

---
+++ src/main/java/me/mocha/planther/common/model/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package me.mocha.planther.common.model.repository;
+
+import me.mocha.planther.common.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
