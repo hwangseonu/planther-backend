d87d89fa38cf02c2df9e6ac4546b8c29f1f224e6
hwangseonu <hwangseonu12@naver.com>
2018-12-25 21:36:35 +0900

    feat: Add SignUpController

--- src/main/java/me/mocha/planther/user/request/SignUpRequest.java
+++ src/main/java/me/mocha/planther/user/request/SignUpRequest.java
@@ -15,7 +15,7 @@ public class SignUpRequest {
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
-    @Range(min = 1, max = 3, message = "학년의 범위를 벗어났습니다.")
+    @Range(min = 1, max = 3, message = "학년이 범위를 벗어났습니다.")
     private Integer grade;
 
     @Range(min = 1, max = 4, message = "1반부터 4반까지만 있습니다.")
