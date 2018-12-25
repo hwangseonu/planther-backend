d425b16497c54c5329130793650408eced8ef714
hwangseonu <hwangseonu12@naver.com>
2018-12-25 22:37:35 +0900

    feat: Add jwt authentication for security

--- src/main/java/me/mocha/planther/common/config/SecurityConfig.java
+++ src/main/java/me/mocha/planther/common/config/SecurityConfig.java
@@ -1,16 +1,47 @@
 package me.mocha.planther.common.config;
 
+import me.mocha.planther.common.security.jwt.JwtAuthenticationEntryPoint;
+import me.mocha.planther.common.security.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public SecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and();
+        http.csrf().disable().cors().and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/me/mocha/planther/common/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package me.mocha.planther.common.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import me.mocha.planther.common.exception.NotFoundException;
+import me.mocha.planther.common.model.repository.UserRepository;
+import me.mocha.planther.common.security.user.UserDetailsServiceImpl;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider, UserRepository userRepository, UserDetailsServiceImpl userDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.userRepository = userRepository;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            String token = getTokenFromRequestHeader(request);
+            if (StringUtils.hasText(token) && jwtProvider.validToken(token, JwtType.ACCESS)) {
+                String username = jwtProvider.getUsernameFromToken(token);
+                if (!userRepository.existsById(username)) {
+                    throw new NotFoundException("존재하지 않는 사용자입니다.");
+                }
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            log.error("Could not set user authentication is security context - {}", e.getMessage());
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequestHeader(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (StringUtils.hasText(header) && header.startsWith("Bearer")) {
+            return header.replaceFirst("Bearer", "").trim();
+        }
+        return "";
+    }
+
+}
