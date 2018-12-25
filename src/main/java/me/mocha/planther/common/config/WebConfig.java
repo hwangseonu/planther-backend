4580b52e2ea39b4da66e70b5b978065ffd5e34fc
hwangseonu <hwangseonu12@naver.com>
2018-12-26 01:14:19 +0900

    feat: Allow cors

--- src/main/java/me/mocha/planther/common/config/WebConfig.java
+++ src/main/java/me/mocha/planther/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package me.mocha.planther.common.config;
 import me.mocha.planther.common.resolver.UserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,6 +17,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     public WebConfig(UserArgumentResolver userArgumentResolver) {
         this.userArgumentResolver = userArgumentResolver;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
     }
 
     @Override
