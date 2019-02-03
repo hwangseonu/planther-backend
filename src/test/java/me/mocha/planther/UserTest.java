b2df71b1baa6e1e057b0cc2c06de744d71117694
hwangseonu <hwangseonu12@naver.com>
2019-02-03 22:43:38 +0900

    test: Add user test class

--- src/test/java/me/mocha/planther/UserTest.java
+++ src/test/java/me/mocha/planther/UserTest.java
@@ -1,5 +1,8 @@
 package me.mocha.planther;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.mocha.planther.user.request.SignInRequest;
+import me.mocha.planther.user.request.SignUpRequest;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -23,38 +26,45 @@ public class UserTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void test1SignInNotFound() throws Exception {
+        SignInRequest request = new SignInRequest("test", "test1234");
         mockMvc.perform(post("/auth")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"test\", \"password\": \"test1234\"}")
+                .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     public void test2SignUpSuccess() throws Exception {
+        SignUpRequest request = new SignUpRequest("test", "test1234", "테스트", 1, 1, 1);
         mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"test\", \"password\": \"test1234\", \"name\": \"테스트\", \"grade\": 1, \"cls\": 1, \"number\": 1}")
+                .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
 
     @Test
     public void test3SignUpConflict() throws Exception {
+        SignUpRequest request = new SignUpRequest("test", "test1234", "테스트", 1, 1, 1);
         mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"test\", \"password\": \"test1234\", \"name\": \"테스트\", \"grade\": 1, \"cls\": 1, \"number\": 1}")
+                .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isConflict());
     }
 
     @Test
     public void test4SignInSuccess() throws Exception {
+        SignInRequest request = new SignInRequest("test", "test1234");
         mockMvc.perform(post("/auth")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"test\", \"password\": \"test1234\"}")
+                .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
