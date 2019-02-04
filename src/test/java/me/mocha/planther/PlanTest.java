83f95b6e7f976d47b8a22f83900c7ab5d7109d2a
hwangseonu <hwangseonu12@naver.com>
2019-02-04 23:49:06 +0900

    feat: Add Test code of CRUD

--- src/test/java/me/mocha/planther/PlanTest.java
+++ src/test/java/me/mocha/planther/PlanTest.java
@@ -16,7 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -42,6 +43,13 @@ public class PlanTest {
                 .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+
+        request = new SignUpRequest("test1", "test1234", "테스트", 2, 2, 2);
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
     }
 
     @Test
@@ -57,15 +65,83 @@ public class PlanTest {
     }
 
     @Test
-    public void test1NewPlanSuccess() throws Exception {
+    public void test2NewPlanSuccess() throws Exception {
         String access = jwtProvider.generateToken("test", JwtType.ACCESS);
-        AddPlanRequest request = new AddPlanRequest("test", "testContent", "event", 2019, 1, 1);
+        AddPlanRequest request = new AddPlanRequest("test", "test", "event", 2019, 1, 1);
         mockMvc.perform(post("/plans")
                 .header("Authorization", "Bearer " + access)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("test"))
+                .andExpect(jsonPath("$.content").value("test"))
+                .andExpect(jsonPath("$.type").value("event"))
+                .andExpect(jsonPath("$.year").value(2019))
+                .andExpect(jsonPath("$.month").value(1))
+                .andExpect(jsonPath("$.day").value(1))
+                .andExpect(jsonPath("$.classId").value("101"));
+    }
+
+    @Test
+    public void test3GetPlanNotFound() throws Exception {
+        String access = jwtProvider.generateToken("test", JwtType.ACCESS);
+        mockMvc.perform(get("/plans/2")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void test4GetPlanForbidden() throws Exception {
+        String access = jwtProvider.generateToken("test1", JwtType.ACCESS);
+        mockMvc.perform(get("/plans/1")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void test5GetPlanSuccess() throws Exception {
+        String access = jwtProvider.generateToken("test", JwtType.ACCESS);
+        mockMvc.perform(get("/plans/1")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("test"))
+                .andExpect(jsonPath("$.content").value("test"))
+                .andExpect(jsonPath("$.type").value("event"))
+                .andExpect(jsonPath("$.year").value(2019))
+                .andExpect(jsonPath("$.month").value(1))
+                .andExpect(jsonPath("$.day").value(1))
+                .andExpect(jsonPath("$.classId").value("101"));
+    }
+
+    @Test
+    public void test6DeletePlanNotFound() throws Exception {
+        String access = jwtProvider.generateToken("test", JwtType.ACCESS);
+        mockMvc.perform(delete("/plans/2")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void test7DeletePlanForbidden() throws Exception {
+        String access = jwtProvider.generateToken("test1", JwtType.ACCESS);
+        mockMvc.perform(delete("/plans/1")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void test8DeletePlanSuccess() throws Exception {
+        String access = jwtProvider.generateToken("test", JwtType.ACCESS);
+        mockMvc.perform(delete("/plans/1")
+                .header("Authorization", "Bearer " + access)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
