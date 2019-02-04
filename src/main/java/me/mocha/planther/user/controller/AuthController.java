2d3a38788698b3d471fc36d34c926a44bec679ff
hwangseonu <hwangseonu12@naver.com>
2019-02-04 14:23:11 +0900

    feat: Add refresh token endpoint

--- src/main/java/me/mocha/planther/user/controller/AuthController.java
+++ src/main/java/me/mocha/planther/user/controller/AuthController.java
@@ -11,12 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/auth")
@@ -49,6 +49,24 @@ public class AuthController {
         return ResponseEntity.ok(new SignInResponse(
                 jwtProvider.generateToken(user.getUsername(), JwtType.ACCESS),
                 jwtProvider.generateToken(user.getUsername(), JwtType.REFRESH)));
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<SignInResponse> refresh(@RequestHeader("Authorization") String header) {
+        if (header.startsWith("Bearer")) {
+            String token = header.replaceFirst("Bearer", "").trim();
+            if (StringUtils.hasText(token) && jwtProvider.validToken(token, JwtType.REFRESH)) {
+                String username = jwtProvider.getUsernameFromToken(token);
+                if (userRepository.existsById(username)) {
+                    SignInResponse response = new SignInResponse(jwtProvider.generateToken(username, JwtType.ACCESS), null);
+                    if (ChronoUnit.DAYS.between(LocalDate.now(), jwtProvider.getExpiration(token)) <= 7) {
+                        response.setRefresh(jwtProvider.generateToken(username, JwtType.REFRESH));
+                    }
+                    return ResponseEntity.ok(response);
+                }
+            }
+        }
+        return ResponseEntity.badRequest().build();
     }
 
 }
